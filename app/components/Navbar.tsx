9b877d50327dbadf9eca3fcb8b60ff906f38aa5b
piyushpaliwalcodes <piyushpaliwalofficial868@gmail.com>
2025-09-21 15:28:33 +0530

    image also added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,23 @@
 
-const Navbar = ()=>
-{
-    return <div className="flex px-6 py-2 border-[#2C593C6B] border-b-[1px]">
-        <img src="/uttamseeds.png" className="w-[110px] h-[60px]" alt="Uttam Seeds Logo"/>
-    </div>
+const Navbar = () => {
+    return (
+        <div className="flex items-center justify-between pl-6 py-2 border-[#2C593C6B] border-b-[1px] bg-[#fff0e0]">
+            <img src="/uttamseeds.png" className="w-[110px] h-[60px]" alt="Uttam Seeds Logo"/>
+            <div 
+                className="flex items-center justify-center px-4 py-1 text-white font-semibold text-sm"
+                style={{
+                    backgroundImage: `url("/ribbon.png")`,
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center',
+                    minWidth: '250px',
+                    height: '60px'
+                }}
+            >
+                <p className="text-center text-[#2C593C] font-semibold">Crop Guide / फसल मार्गदर्शिका</p>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar;
