8e978f8b609d2a75832f01ae14ddb47dcd90415a
piyushpaliwalcodes <piyushpaliwalofficial868@gmail.com>
2025-09-24 23:55:47 +0530

    changes done

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,7 @@
 const Navbar = () => {
     return (
         <div className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between pl-6 py-2 border-[#2C593C6B] border-b-[1px] bg-[#fff0e0]">
-            <img src="/kesaribeej.png" className="w-[80px] h-[60px]" alt="Kesari beej Logo"/>
+            <img src="/kesaribeej.png" className="w-[70px] h-[60px]" alt="Kesari beej Logo"/>
             <div 
                 className="flex items-center justify-center px-4 py-1 text-white font-semibold text-sm"
                 style={{
